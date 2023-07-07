fe57387e15c3b32d928b56ecee15d9082dfabb8f
niklas051299 <n-klinzing@t-online.de>
2023-07-07 12:00:42 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { MainViewComponent } from './main-view/main-view.component';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import { ArtistSearchComponent } from './artist-search/artist-search.component';
+import { SideBySideComparisonComponent } from './side-by-side-comparison/side-by-side-comparison.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainViewComponent },
+  { path: 'detail/:artistId', component: DetailViewComponent },
+  { path: 'search', component: ArtistSearchComponent },
+  { path: 'comparison', component: SideBySideComparisonComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainViewComponent } from './main-view/main-view.component';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import { ArtistSearchComponent } from './artist-search/artist-search.component';
+import { SideBySideComparisonComponent } from './side-by-side-comparison/side-by-side-comparison.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainViewComponent,
+    DetailViewComponent,
+    ArtistSearchComponent,
+    SideBySideComparisonComponent
   ],
   imports: [
     BrowserModule,
